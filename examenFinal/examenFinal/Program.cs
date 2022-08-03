ec79ae85727d19697b584bf4b1cdbb1be979d274
Crismedina07 <41929688+Crismedina07@users.noreply.github.com>
2022-08-03 13:10:42 -0400

    Adicción de manejo de errores

--- examenFinal/examenFinal/Program.cs
+++ examenFinal/examenFinal/Program.cs
@@ -10,41 +10,56 @@ namespace examenFinal
     {
         static void Main(string[] args)
         {
-            int acumulado = 0;
-            int caso = 0;
-            
-            Console.Write("Ingrese el acumulado del primer parcial: ");
-            acumulado = int.Parse(Console.ReadLine());
-            Console.Write("Ingrese el acumulado del segundo parcial: ");
-            acumulado = acumulado + int.Parse(Console.ReadLine());
-            Console.Write("Ingrese el acumulado del examen final: ");
-            acumulado = acumulado + int.Parse(Console.ReadLine());
-
-            switch (acumulado)
+            inicio:
+            try
             {
-                case int n when (n == 0 && n <= 59):
-                    Console.WriteLine($"La calificacion es: {n} y la calificación alfabetica es F");
-                    break;
-                case int n when (n >= 60 && n <= 69):
-                    Console.WriteLine($"La calificacion es: {n} y la calificación alfabetica es D");
-                    break;
-                case int n when (n >= 70 && n <= 79):
-                    Console.WriteLine($"La calificacion es: {n} y la calificación alfabetica es C");
-                    break;
-                case int n when (n >= 80 && n <= 89):
-                    Console.WriteLine($"La calificacion es: {n} y la calificación alfabetica es B");
-                    break;
-                case int n when (n >= 90 && n <= 100):
-                    Console.WriteLine($"La calificacion es: {n} y la calificación alfabetica es A");
-                    break;
+                int acumulado = 0;
+                string decision;
+
+                Console.Write("Ingrese el acumulado del primer parcial: ");
+                acumulado = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el acumulado del segundo parcial: ");
+                acumulado = acumulado + int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el acumulado del examen final: ");
+                acumulado = acumulado + int.Parse(Console.ReadLine());
+
+                switch (acumulado)
+                {
+                    case int n when (n == 0 && n <= 59):
+                        Console.WriteLine($"La calificacion es: {n} y la calificación alfabetica es F");
+                        break;
+                    case int n when (n >= 60 && n <= 69):
+                        Console.WriteLine($"La calificacion es: {n} y la calificación alfabetica es D");
+                        break;
+                    case int n when (n >= 70 && n <= 79):
+                        Console.WriteLine($"La calificacion es: {n} y la calificación alfabetica es C");
+                        break;
+                    case int n when (n >= 80 && n <= 89):
+                        Console.WriteLine($"La calificacion es: {n} y la calificación alfabetica es B");
+                        break;
+                    case int n when (n >= 90 && n <= 100):
+                        Console.WriteLine($"La calificacion es: {n} y la calificación alfabetica es A");
+                        break;
+                }
+                Console.Write("Desea procesar otro la calificación de otro estudiante? ");
+                decision = Console.ReadLine();
+                switch (decision)
+                {
+                    case "s":
+                        goto inicio;
+                        break;
+                    case "n":
+                        Environment.Exit(0);
+                        break;
+                }
+
             }
-            Console.ReadKey();
-            //if (acumulado = 0 && >= )
-            //{
+            catch (Exception ex)
+            {
 
-            //}
-
-
+                Console.WriteLine("Ha ocurrido un error inesperado: " + ex);
+                goto inicio;
+            }
         }
     }
 }
